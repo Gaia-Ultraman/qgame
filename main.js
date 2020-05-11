8d6858d49cae08e74e0f1627f297f518ed5df28e
liqiang <1281991712@qq.com>
2020-05-11 23:06:52 +0800

    Signed-off-by: liqiang <1281991712@qq.com>

--- main.js
+++ main.js
@@ -393,11 +393,6 @@ function checkAgency(name) {
     log(2)
     if (bt1) {
         log("@@成功")
-        log("QQ启动状态:", launch("com.tencent.mobileqq"));
-        if (textContains("正在尝试开启").findOne(3000)) {
-            text("允许").click()
-        }
-        sleep(8000)
         if (oldStatus == "inRoom" && time == 0) {
             log("代理运行正常")
             launchApp("企鹅电竞")
@@ -406,6 +401,11 @@ function checkAgency(name) {
             }
         } else {
             log("代理重连后运行正常")
+            log("QQ启动状态:", launch("com.tencent.mobileqq"));
+            if (textContains("正在尝试开启").findOne(3000)) {
+                text("允许").click()
+            }
+            sleep(8000)
             OpenToRoom(name)
         }
         appStatus = oldStatus

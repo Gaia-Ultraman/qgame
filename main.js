7654c152fcdad82fc359c9f2e3d2de833313e49e
liqiang <1281991712@qq.com>
2020-05-16 11:17:13 +0800

    1

--- main.js
+++ main.js
@@ -460,16 +460,16 @@ return function (arr) {
         tdSecond += 24 * 3600
     }
     //有可能直播在今晚上12点前结束，重新进入计算时，当时时间已经超过了最后一位主播的下拨时间，计算第二天的时间
-    let cloneArr=JSON.parse(JSON.stringify(arr))
-    if(arr[2].end * 3600 < tdSecond){
-        cloneArr=arr.map(v=>{
-            return {
-                name=v.name,
-                start=v.start+24*3600,
-                end=v.end+24*3600
-            }
-        })
-    }
+    // let cloneArr=JSON.parse(JSON.stringify(arr))
+    // if(arr[2].end * 3600 < tdSecond){
+    //     cloneArr=arr.map(v=>{
+    //         return {
+    //             name=v.name,
+    //             start=v.start+24*3600,
+    //             end=v.end+24*3600
+    //         }
+    //     })
+    // }
 
     //把传入的小时时间全部转换为秒数
     cloneArr.forEach((v, i) => {
